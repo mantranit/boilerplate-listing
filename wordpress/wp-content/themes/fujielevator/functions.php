a72859c1b3dd728f1ff477144394dfc10f6efcdf
Jimmy Tran <jimmy.tran@watasolutions.com>
2019-08-09 15:36:36 +0700

    update header

--- wordpress/wp-content/themes/fujielevator/functions.php
+++ wordpress/wp-content/themes/fujielevator/functions.php
@@ -38,10 +38,35 @@ function fujielevator_assets() {
 }
 add_action('wp_enqueue_scripts', 'fujielevator_assets');
 
-/* TODO: override parent functions */
-//function storefront_product_search() {}
+/* override parent functions */
 function storefront_header_cart() {}
 function storefront_primary_navigation_wrapper() {}
 function storefront_primary_navigation_wrapper_close() {}
 function storefront_handheld_footer_bar() {}
-function storefront_product_search() {}
+
+
+function storefront_product_search() {
+    if ( storefront_is_woocommerce_activated() ) {
+        ?>
+        <input type="checkbox" id="toggleSearchBar" name="toggleSearchBar">
+        <label for="toggleSearchBar" class="toggleSearchBar-label"></label>
+        <div class="site-search">
+            <?php the_widget( 'WC_Widget_Product_Search', 'title=' ); ?>
+        </div>
+        <?php
+    }
+}
+
+add_filter( 'get_product_search_form' , 'woo_custom_product_searchform' );
+function woo_custom_product_searchform( $form ) {
+
+    $form = '<form role="search" method="get" id="searchform" action="' . esc_url( home_url( '/'  ) ) . '">
+		<div class="search-product">
+			<input type="text" value="' . get_search_query() . '" name="s" id="s" placeholder="' . __( 'Search', 'fujielevator' ) . '" />
+			<input type="submit" id="searchsubmit" value="'. esc_attr__( 'Search', 'fujielevator' ) .'" />
+			<input type="hidden" name="post_type" value="product" />
+		</div>
+	</form>';
+    return $form;
+
+}
