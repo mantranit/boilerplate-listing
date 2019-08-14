fa13e890f9c6e0269455aeef8fb9094ae2633e2c
Jimmy Tran <jimmy.tran@watasolutions.com>
2019-08-14 16:20:03 +0700

    product update

--- app/js/modules/DelegateClickGallery.js
+++ app/js/modules/DelegateClickGallery.js
@@ -1,6 +1,8 @@
 export default class DelegateClickGallery {
   constructor(el) {
-    $('.wp-block-gallery figure').click(function() {
+    $('.wp-block-gallery figure').click(function(e) {
+      e.preventDefault();
+
       let link = $(this).find('figcaption a');
       if (link.length > 0 && link.attr('href') && link.attr('href') !== '#') {
         window.location.href = link.attr('href');
